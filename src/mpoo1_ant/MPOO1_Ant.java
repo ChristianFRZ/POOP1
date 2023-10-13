78ac72c6d69d624ceb28a185424cdacda93aff13
ChristianFRZ <144077756+ChristianFRZ@users.noreply.github.com>
2023-10-12 20:41:43 -0600

    Creación Ant

---
+++ src/mpoo1_ant/MPOO1_Ant.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mpoo1_ant;
+
+/**
+ *
+ * @author sonic
+ */
+public class MPOO1_Ant {
+
+    /**
+     * Sirve para imprimir la cadena "Hola mundo desdde POO grupo 8"
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola mundo desde POO grupo 8");
+    }
+    
+}
